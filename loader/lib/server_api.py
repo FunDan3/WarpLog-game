5ca9cc9aee3d03c5824efeedc48485a8faee1504
Fun_Dan3 <dfr34560@gmail.com>
2024-03-23 16:11:52 +0300

    Added client support for recursive downloadign

--- loader/lib/server_api.py
+++ loader/lib/server_api.py
@@ -1,4 +1,6 @@
 from .aconn import aconn
+from aiofiles import os as aos
+import aiofiles
 import asyncio
 import pygame
 import json
@@ -6,12 +8,6 @@ import os
 
 pygame.font.init()
 pygame.mixer.init()
-
-class folder:
-	def __init__(self, **kwargs):
-		for name, value in kwargs:
-			setattr(self, name, value)
-
 code_map = {
 "close": 0,
 "load_assets": 1,
@@ -22,12 +18,18 @@ code_bytes = 2
 for key, value in code_map.items():
 	code_map[key] = value.to_bytes(code_bytes, "big", signed = False)
 
-def get_loading_method(filename):
-	ext = os.path.splitext(filename)[1]
-	for method, file_formats in loading_methods.items():
-		if ext in file_formats:
-			return method
-	return "%s"
+async def ensure_path(file):
+	past_dir = ""
+	for dir in file.split("/")[:len(file.split("/"))-1]:
+		try:
+			await aos.mkdir(past_dir+dir)
+		except FileExistsError:
+			pass
+		past_dir += dir + "/"
+async def write_file(path, content):
+	await ensure_path(path)
+	async with aiofiles.open(path, "wb") as f:
+		await f.write(content)
 
 class api:
 	ip = None
@@ -41,18 +43,20 @@ class api:
 	async def connect(self):
 		self.connection = aconn()
 		await self.connection.connect(self.ip, self.port)
-	async def load_assets(self):
+	async def load_assets(self, path):
 		await self.connection.write(code_map["load_assets"])
-		json_size = int.from_bytes(await self.connection.read(16), "big", signed = False)
+		json_size = int.from_bytes(await self.connection.read(32), "big", signed = False)
 		json_data = json.loads(await self.connection.read(json_size))
-		total_files_size = json_data["total_files_size"]
+		total_files_size = json_data["total_size"]
 		loaded_files_size = 0
-		assets = folder()
-		for file_path, file_size in json_data["asset_sizes"].items():
+		event_loop = asyncio.get_event_loop()
+		tasks = []
+		for file_path, file_size in json_data["file_sizes"].items():
 			loaded_files_size += file_size
-			print(f"Loading asset {file_path} {round(loaded_files_size/total_files_size*100, 2)}%")
-			asset_name = os.path.splitext(file_path)[0].split("/")[-1]
-			asset_content = await self.connection.read(file_size)
-			asset_content = eval("%s" % "asset_content")
-			setattr(assets, asset_name, asset_content)
-		return assets
+			print(f"Downloading file {file_path} {round(loaded_files_size/total_files_size*100, 2)}%")
+			file_path = file_path.replace("./", path)
+
+			file_content = await self.connection.read(file_size)
+			tasks.append(write_file(file_path, file_content))
+		print(f"Writing files...")
+		await asyncio.gather(*tasks)
--- loader/main.py
+++ loader/main.py
@@ -7,9 +7,6 @@ from lib import server_api
 async def start_game():
 	api = server_api.api("localhost")
 	await api.connect()
-	assets = await api.load_assets()
-	for attr in dir(assets):
-		if not attr.startswith("_"):
-			print(getattr(assets, attr))
+	await api.load_assets(path = "./game/")
 
 asyncio.run(start_game())
