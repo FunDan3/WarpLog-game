36942a24e588fc10221479fcaa770dbd1a3e32d0
Fun_Dan3 <dfr34560@gmail.com>
2024-04-04 18:49:21 +0300

    Updated asset map again

--- client/main.py
+++ client/main.py
@@ -13,15 +13,18 @@ renderer = renderer.renderer()
 
 asset_map = {
 	"fonts.QuantumLemon": ("pygame.font.Font('%s', 128)", "./assets/fonts/QuantumLemon.ttf"),
-	"image.credit": ("pygame.image.load('%s')", "./assets/images/credit.png"),
+	"images.credit": ("pygame.image.load('%s')", "./assets/images/credit.png"),
 	"music.menu": random.choice([("pygame.mixer.Sound('%s')", "./assets/music/menu1.mp3"), ("pygame.mixer.Sound('%s')", "./assets/music/menu2.mp3")]),
 	"sfx.cancel": ("pygame.mixer.Sound('%s')", "./assets/sfx/cancel.mp3"),
 	"sfx.error": ("pygame.mixer.Sound('%s')", "./assets/sfx/error.mp3"),
-	"sfx.success": ("pygame.mixer.Sound('%s')", "./assets/sfx/success.mp3")
+	"sfx.success": ("pygame.mixer.Sound('%s')", "./assets/sfx/success.mp3"),
+	"sfx.ui_input": ("pygame.mixer.Sound('%s')", "./assets/sfx/ui_input.mp3"),
+	"sfx.warning": ("pygame.mixer.Sound('%s')", "./assets/sfx/warning.mp3")
 }
 
 async def window_switcher():
 	assets = await windows.load_assets(renderer, asset_map)
+	await windows.login_menu(renderer, assets)
 async def main():
 	await asyncio.gather(renderer.loop(), window_switcher())
 
