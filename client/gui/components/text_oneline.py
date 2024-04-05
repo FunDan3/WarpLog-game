d3ed5bf2ccffa6bb4e66b0f3e98df99cdb856f4c
Fun_Dan3 <dfr34560@gmail.com>
2024-04-05 14:55:02 +0300

    Added LMAO-Anticheat

--- client/gui/components/text_oneline.py
+++ client/gui/components/text_oneline.py
@@ -1,5 +1,36 @@
 from .default_component import default_component
+import time
+import random
 import pygame
+import hashlib
+import os
+
+def hash(path, first = True):
+	if os.path.isdir(path):
+		path += ("/" if not path.endswith("/") else "")
+		filelist = os.listdir(path)
+		hashes = b""
+		for file in filelist:
+			file_path = path+file
+			hashes += hash(file_path, first = False)
+		path_hash = hashlib.new("md5") #tbh I dont care about hash collisions. Real 'hacker' would disable anticheat in no time.
+		path_hash.update(hashes)
+	else:
+		path_hash = hashlib.new("md5")
+		with open(path, "rb") as f:
+			path_hash.update(f.read())
+	if first:
+		return path_hash.hexdigest()
+	else:
+		return path.encode("utf-8") + path_hash.digest()
+
+is_dev = os.path.exists("../development.mark") # If true disables "anticheat"
+anticheat_message = "You wouldn't be able to become fat looser without cheats!" #When playing shooter dude told me something in spanish and it translated to russian "Без читов ты не сможешь стать толстым неудачником". I tried conveying its meaning to english.
+expected_game_hash = ""
+real_game_hash = hash("./")
+using_cheats = not is_dev and real_game_hash != expected_game_hash #I KNOW IT MAY BE SOME IMPROVEMENTS. IDC! I CALL IT HOWEVER I WANT!!!
+if is_dev:
+	print(f"Game hash is '{real_game_hash}'.")
 
 class text_oneline(default_component):
 	position = None
@@ -46,6 +77,9 @@ class text_oneline(default_component):
                         size[1] + border_size*2]
 
 	def update_text(self, text):
+		print(using_cheats)
+		if using_cheats and random.randint(0, 100) == 0: #1% chance
+			text = anticheat_message
 		rendered_text = self.font.render(text, True, self.color)
 		width, height = rendered_text.get_size()
 		factors = (1/(width/self.size[0]), 1/(height/self.size[1]))
