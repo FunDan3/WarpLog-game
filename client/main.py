35a08c744382b6df5ee47b21d3a956ec25d65fcc
Fun_Dan3 <dfr34560@gmail.com>
2024-04-07 19:32:47 +0300

    Added new font

--- client/main.py
+++ client/main.py
@@ -12,6 +12,7 @@ pygame.mixer.init()
 renderer = renderer.renderer()
 
 asset_map = {
+	"fonts.AndroidInsomnia": ("pygame.font.Font('%s', 128)", "./assets/fonts/AndroidInsomnia.ttf"),
 	"fonts.QuantumLemon": ("pygame.font.Font('%s', 128)", "./assets/fonts/QuantumLemon.ttf"),
 	"images.credit": ("pygame.image.load('%s')", "./assets/images/credit.png"),
 	"music.menu": random.choice([("pygame.mixer.Sound('%s')", "./assets/music/menu1.mp3"), ("pygame.mixer.Sound('%s')", "./assets/music/menu2.mp3")]),
@@ -23,8 +24,9 @@ asset_map = {
 }
 
 async def window_switcher():
-	assets = await windows.load_assets(renderer, asset_map)
-	await windows.login_menu(renderer, assets)
+	assets, connection = await windows.load_assets(renderer, asset_map)
+	await windows.login_menu(renderer, assets, connection) #I will add tls later
+
 async def main():
 	await asyncio.gather(renderer.loop(), window_switcher())
 
