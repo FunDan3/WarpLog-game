188bfc5ad137d4f197e3e3e6b4efaecbe9382174
Fun_Dan3 <dfr34560@gmail.com>
2024-04-08 18:40:57 +0300

    Made login menu interactive

--- client/windows/login_menu.py
+++ client/windows/login_menu.py
@@ -11,5 +11,27 @@ async def login_menu(renderer, assets, connection):
 	password_field = components.text_input(position = [32, 256+64], size = [512-64, 128], default_text = "Password", font = assets.fonts.AndroidInsomnia, on_enter = lambda: login_button.on_click())
 	login_field = components.text_input(position = [32, 128+32], size = [512-64, 128], default_text = "Login", font = assets.fonts.AndroidInsomnia, on_enter = lambda: password_field.activate())
 	register_button = components.text_button(position = [(512-64)//2+64, 384 + 96], size = [(512-64)//2-32, 64], text = "Sign up", font = assets.fonts.QuantumLemon)
+
+	async def authenticate(login, password, registration):
+		login_button.interactive = False
+		register_button.interactive = False
+		status_box.visible = False
+
+		error = await connection.authenticate(login, password, registration) #if fails returns error else None
+
+		status_box.visible = True
+		login_button.interactive = True
+		register_button.interactive = True
+
+	def on_login():
+		assets.sfx.ui_input.play()
+		asyncio.ensure_future(authenticate(login = login_field.input_text, password = password_field.input_text, registration = False), loop = asyncio.get_running_loop())
+	def on_register():
+		assets.sfx.ui_input.play()
+		asyncio.ensure_future(authenticate(login = login_field.input_text, password = password_field.input_text, registration = False), loop = asyncio.get_running_loop())
+
+	login_button.on_click = on_login
+	register_button.on_click = on_register
+
 	window.add_components(title, status_box, login_field, password_field, login_button, register_button)
 	renderer.add_component(window)
